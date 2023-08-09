b6b51d536f031836742a35fcbdbef3a738d11ffb
Cecilia Baronio <baronio_cecilia@hotmail.com>
2023-08-09 14:06:18 -0300

    menu.sh

--- menu.sh
+++ menu.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 # Menú de opciones
 while true; do
-    echo "===== MENÚ ====="
-    echo "1. Generar imágenes"
-    echo "2. Descargar imágenes"
-    echo "3. Descomprimir imágenes"
-    echo "4. Procesar imágenes"
-    echo "5. Comprimir archivos"
-    echo "6. Salir"
+	echo "===== MENÚ ====="
+	echo "1. Generar imágenes"
+	echo "2. Descargar imágenes"
+	echo "3. Descomprimir imágenes"
+	echo "4. Procesar imágenes"
+	echo "5. Procesar miniaturas"
+	echo "6. Comprimir archivos"
+	echo "7. Salir"
 
     read -p "Ingrese el número de opción: " opcion
 
     case $opcion in
         1)
-            read -p "Ingrese la cantidad de imágenes a generar: " cantidad
-            source generar.sh "$cantidad"
-            ;;
+		read -p "Ingrese la cantidad de imágenes a generar: " cantidad
+		source generar.sh "$cantidad"
+            	;;
         2)
-            read -p "Ingrese por argumentos dos URL (una para las imágenes y otra para la suma de verificación): " url_imagenes url_sumaverificacion
-            source descargar.sh "$url_imagenes" "$url_sumaverificacion"
-            ;;
+           	read -p "Ingrese por argumentos dos URL (una para las imágenes y otra para la suma de verificación): " url_imagenes url_sumaverificacion
+            	source descargar.sh "$url_imagenes" "$url_sumaverificacion"
+            	;;
         3)
-            source descomprimir.sh
-            ;;
+            	source descomprimir.sh
+           	;;
         4)
-            source procesar.sh
-            ;;
+           	 source procesar.sh
+            	;;
         5)
-            source comprimir.sh
-            ;;
-        6)
-            echo "Saliendo..."
-            break
-            ;;
+            	source comprimir.sh
+            	;;
+	6) 
+	   	source miniaturas.sh
+		;;
+        7)
+           	 echo "Saliendo..."
+            	break
+           	 ;;
         *)
             echo "Opción inválida. Intente nuevamente."
             ;;
