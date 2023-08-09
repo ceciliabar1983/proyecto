ed2f8c2a63143d8e48c14d1d2af24e609dde5f46
Cecilia Baronio <baronio_cecilia@hotmail.com>
2023-08-09 10:02:15 -0300

    miniaturas.sh

---
+++ miniaturas.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Verificar si existe la carpeta "imagenes.zip" en la ubicación actual
+if [ -f imagenes.zip ]; then
+    # Descomprimir el archivo imagenes.zip en la carpeta "imagenes_descomprimidas_con_miniaturas"
+    if [ -d imagenes_descomprimidas_con_miniaturas ]; then
+        rm -rf imagenes_descomprimidas_con_miniaturas 2>/dev/null
+    fi
+    unzip imagenes.zip -d imagenes_descomprimidas_con_miniaturas
+    
+    # Crear la carpeta "imagenes_procesadas_con_miniaturas"
+    if [ -d imagenes_procesadas_con_miniaturas ]; then
+        rm -rf imagenes_procesadas_con_miniaturas 2>/dev/null
+    fi
+    mkdir -p imagenes_procesadas_con_miniaturas
+
+    # Modificar el tamaño a miniatura
+    for archivo in imagenes_descomprimidas_con_miniaturas/imagenes/*.jpg ; do
+        nombre_archivo=$(basename "$archivo")
+        nombre_persona="${nombre_archivo%.*}"
+
+        # Redimensionar la imagen a 48x48 y guardarla en la carpeta "imagenes_procesadas_con_miniaturas"
+        convert "$archivo" -resize 48x48 "imagenes_procesadas_con_miniaturas/${nombre_persona}_imagen_con_miniaturas.jpg"
+    done
+
+    # Comprimir las imágenes procesadas en un nuevo archivo "imagenes_procesadas_con_miniaturas.zip"
+    if [ -d imagenes_procesadas_con_miniaturas ]; then
+        zip -r imagenes_procesadas_con_miniaturas.zip imagenes_procesadas_con_miniaturas
+    fi
+fi
